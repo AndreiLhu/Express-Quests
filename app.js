633e264319539d66780b0c90c513ad21536dfc60
AndreiLhu <leahu.andreilucian@gmail.com>
2023-01-02 13:33:51 +0100

    Creating users branch

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 
 const app = express();
 
 const port = process.env.APP_PORT ?? 5001;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send('Welcome User');
 };
 
-app.get("/", welcome);
+app.get('/', welcome);
 
-const movieHandlers = require("./movieHandlers");
+const userHandlers = require('./userHandlers');
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get('/api/users', userHandlers.getUsers);
+app.get('/api/users/:id', userHandlers.getUserById);
 
 app.listen(port, (err) => {
   if (err) {
-    console.error("Something bad happened");
+    console.error('Something bad happened');
   } else {
     console.log(`Server is listening on ${port}`);
   }
