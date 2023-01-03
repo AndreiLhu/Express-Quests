c963f58a809ea544efcafd37b4846ebdc9c34067
AndreiLhu <leahu.andreilucian@gmail.com>
2023-01-03 10:57:43 +0100

    Adding post user

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-require('dotenv').config();
-const express = require('express');
+
+const express = require("express");
 
 const app = express();
+
+app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5001;
 
 const welcome = (req, res) => {
-  res.send('Welcome User');
+  res.send("Welcome to users list");
 };
 
-app.get('/', welcome);
+app.get("/", welcome);
 
-const userHandlers = require('./userHandlers');
 
-app.get('/api/users', userHandlers.getUsers);
-app.get('/api/users/:id', userHandlers.getUserById);
+
+const userHandlers = require("./userHandlers");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", userHandlers.postUser)
+
 
 app.listen(port, (err) => {
   if (err) {
-    console.error('Something bad happened');
+    console.error("Something bad happened");
   } else {
     console.log(`Server is listening on ${port}`);
   }
