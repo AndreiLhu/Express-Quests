40fccaa5e5c7d28b3c937e8f37dfff16ab1fac7d
AndreiLhu <leahu.andreilucian@gmail.com>
2023-01-06 10:19:19 +0100

    creating user/movie validators

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const userHandlers = require('./userHandlers');
 
 app.get('/api/users', userHandlers.getUsers);
 app.get('/api/users/:id', userHandlers.getUserById);
-app.post('/api/users', userHandlers.postUser);
+app.post('/api/users', validateUser, userHandlers.postUser);
 app.put('/api/users/:id', userHandlers.updateUser);
 app.delete('/api/users/:id', userHandlers.deleteUser);
 
@@ -36,6 +36,6 @@ const movieHandlers = require('./movieHandlers');
 
 app.get('/api/movies', movieHandlers.getMovies);
 app.get('/api/movies/:id', movieHandlers.getMovieById);
-app.post('/api/movies', movieHandlers.postMovie);
+app.post('/api/movies', validateMovie, movieHandlers.postMovie);
 app.put('/api/movies/:id', movieHandlers.updateMovie);
 app.delete('/api/movies/:id', movieHandlers.deleteMovie);
